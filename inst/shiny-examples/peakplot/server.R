7f57402e820249f5ce81968996bf14e62d8c9e60
cpanse <cp@fgcz.ethz.ch>
2016-07-04 19:55:29 +0200

    initiial

---
+++ inst/shiny-examples/peakplot/server.R
@@ -0,0 +1,51 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(bibliospec)
+library(specL)
+library(NestLink)
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+  
+  
+  
+  #dataInput <- reactive({
+    
+  # Bibliospec(dbfile=paste(input$DATAROOT, input$file, sep='/'))
+    
+  #})
+  
+  
+  output$distPlot <- renderPlot({
+    #S <- NULL
+    
+    if (input$load){
+    BS <- Bibliospec(dbfile=paste(input$DATAROOT, input$file, sep='/'))
+    S <- BS$getPsmSet()
+    
+    idx <- which(sapply(S, function(x){-10 * log((1E-6 + x$score)) / log(10) > input$mascotScoreCutOff}))
+    }
+  
+    
+   SS <- do.call('rbind',lapply(S[idx], function(x){
+                    data.frame(rt.predicted=ssrc(as.character(x$peptideSequence)), 
+                               rt=x$rt)
+                    }))
+   
+    op <- par(mfrow=c(2,1))
+    #hist(sapply(S, function(x){-10 * log((1E-6 + x$score)) / log(10)}))
+    plot(S[[input$id]])
+    plot(SS$rt , SS$rt.predicted, main=input$file, 
+         sub=paste('number of psm =', length(SS$rt))
+         )
+  })
+  
+})
---
+++ inst/shiny-examples/peakplot/ui.R
@@ -0,0 +1,45 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+DATAROOT <- "/home/cp/__checkouts/R/NestLink/inst/extdata/20160628/"
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("NestLink peptide prediction"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+       textInput("DATAROOT", 
+                 "data directory", 
+                 value=DATAROOT),
+       
+       selectInput('file', 'file', list.files(path=DATAROOT)),
+       actionButton("load", "load"),
+       sliderInput("id", 
+                   "psm id", 
+                   min = 1,
+                   max = 150,
+                   value = 1),
+       
+       sliderInput("mascotScoreCutOff",
+                   "mascot score cut off:",
+                   min = 1,
+                   max = 150,
+                   value = 1)
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+       plotOutput("distPlot", height = "800px")
+    )
+  )
+))
