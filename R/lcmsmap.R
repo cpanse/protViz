8a4a2af389c82fc73d3665754b9944a511e70a78
Christian Panse <cp@fgcz.ethz.ch>
2017-05-26 17:41:03 +0200

    0.2.31 submitted to CRAN

--- R/lcmsmap.R
+++ R/lcmsmap.R
@@ -12,14 +12,13 @@ lcmsmap <- function(data, charges=2:3, score.cutoff = 30, ...){
   s.score <- NULL
   s.charge <- NULL
   
-    if ('mascot' %in% class(data)){
+    if (is.mascot(data)){
       s.rtinseconds <- as.numeric(unlist(lapply(data$queries, function(x){x$RTINSECONDS}))) 
       s.pepmass <- as.numeric(unlist(lapply(data$queries, function(x){x$query_moverz})))
       s.intensity <- as.numeric(lapply(data$queries, function(x){x$TotalIonsIntensity}))
-      s.score <-  (unlist(lapply(data$queries, function(x){rv <- x$q_peptide$pep_score; if(is.null(rv)){NA}else{as.numeric(rv)}})))
+      s.score <- .mascot.get.pep_score(data)
       s.charge <- as.integer(gsub("[+]", "", unlist(lapply(data$queries, function(x){(x$query_charge)}))))
     }else{
-      if (is.null(sub)){sub <- data[[1]]$title}
       s.rtinseconds <- lapply(data, function(x){return (x$rtinseconds)})
       s.pepmass <- lapply(data, function(x){return (x$pepmass)})
       s.intensity <- lapply(data, function(x){return (sum(x$intensity))})
@@ -37,7 +36,6 @@ lcmsmap <- function(data, charges=2:3, score.cutoff = 30, ...){
         plot(S$rt, S$pepmass,
             type='n',
             main=paste('LC-MS overview [', c, ' +]', sep=''),
-            sub=sub,
             xlab='rt [seconds]',
             ylab='pepmass [m/Z]', ...)
       
@@ -51,6 +49,9 @@ lcmsmap <- function(data, charges=2:3, score.cutoff = 30, ...){
         points(S.f$rt, S.f$pepmass, 
             pch=16, col=rgb(0.1,0.1,0.8, alpha=0.4), cex=1)
 
-        legend("topleft", c(paste('score.cutoff = ', score.cutoff), 'rest'), lwd=c(4), col=c(rgb(0.1,0.1,0.8, alpha=0.9), 'grey')) 
+        legend("topleft", c(paste('score.cutoff = ', score.cutoff), 'rest'), 
+               lwd=c(4), 
+               col=c(rgb(0.1,0.1,0.8, alpha=0.9), 
+                     'grey')) 
     }
 }
--- R/mascot.R
+++ R/mascot.R
@@ -1,36 +1,78 @@
 #R
 # Christian Panse <cp@fgcz.ethz.ch> 20170525
 
-.mascot.get.pep_score <- function(data){
-  as.vector(unlist(lapply(data$queries, function(x){rv <- x$q_peptide$pep_score; if(is.null(rv)){NA}else{as.numeric(rv)}})))
-}
 
-.mascot.get.pep_expect <- function(data){
-  as.vector(unlist(lapply(data$queries, function(x){rv <- x$q_peptide$pep_expect; if(is.null(rv)){NA}else{as.numeric(rv)}})))
-}
-
-plot.mascot <- function(data){
-  
-  pep_score <- .mascot.get.pep_score(data)
-  pep_expect <- .mascot.get.pep_expect(data)
-  
-  plot(pep_score, 1/log(pep_expect,10),log='x', pch=16, col=rgb(0.1, 0.1, 0.1, alpha = 0.2))
-  
-  lcmsmap(data)
-}
-
-
-plot.mascot_query <- function(query){
- 
+.mascot.get.ms2 <- function(query){
   S <- lapply(strsplit(query$StringIons1, ","), function(x){strsplit(x, ':')})[[1]]
   
   mZ <- as.numeric(sapply(S, function(x){x[1]}))
   intensity <- as.numeric(sapply(S, function(x){x[2]}))
   idx <- order(mZ)
   
-  spec <- list(mZ=mZ[idx], intensity=intensity[idx])
+  list(mZ=mZ[idx], intensity=intensity[idx])
+}
+
+.mascot.get.pep_score <- function(obj){
+  as.vector(unlist(lapply(obj$queries, function(x){rv <- x$q_peptide$pep_score; if(is.null(rv)){NA}else{as.numeric(rv)}})))
+}
+
+.mascot.get.pep_expect <- function(obj){
+  as.vector(unlist(lapply(obj$queries, function(x){rv <- x$q_peptide$pep_expect; if(is.null(rv)){NA}else{as.numeric(rv)}})))
+}
+
+summary.mascot <- function(object, ...){
+  if (is.mascot(object)){
+    cat("number of queries:\n")
+    cat(paste("\t", length(object$queries), "\n"))
+    cat("quantile pep_score:\n")
+    quantile(.mascot.get.pep_score(object), na.rm=TRUE)
+  }
+  #NextMethod('summary')
+}
+
+is.mascot <- function(obj){
+  if ('mascot' %in% class(obj) 
+      & is.list(obj$queries) 
+      & sum(sapply(obj$queries, is.mascot_query)) == length(obj$queries)){
+    return(TRUE)
+  }
   
-  peakplot(peptideSequence = query$q_peptide$pep_seq, 
-           spec = spec, sub='')
-  # query$q_peptide$pep_seq
+  return(FALSE)
+}
+
+is.mascot_query <- function(obj){
+  mascor_query.names <- c("query_charge", "query_moverz", "SCANS", "StringIons1",
+                          "RTINSECONDS")
+  
+  if ('mascot_query' %in% class(obj) 
+      & length(mascor_query.names) == sum(mascor_query.names %in% names(obj))){
+    return(TRUE)
+  }
+  
+  return(FALSE)
+}
+
+plot.mascot <- function(x, ...){
+  if (is.mascot(x)){
+    pep_score <- .mascot.get.pep_score(x)
+    pep_expect <- .mascot.get.pep_expect(x)
+    
+    plot(pep_score, 1 / log(pep_expect,10),log='x', pch=16, col=rgb(0.1, 0.1, 0.1, alpha = 0.2))
+    
+    lcmsmap(x, ...)
+  }
+  
+ 
+}
+
+
+plot.mascot_query <- function(x, ...){
+ 
+  if (is.mascot_query(x)){
+    spec <- .mascot.get.ms2(x)
+    
+    peakplot(peptideSequence = x$q_peptide$pep_seq, 
+             spec = spec, sub='')
+  }
+
 }
