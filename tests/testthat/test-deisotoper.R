323e1cf5fa42a42ed689c00b00b9f9fdea744254
Christian Panse <cp@fgcz.ethz.ch>
2020-04-23 21:35:54 +0200

    #8 deisotoper

--- tests/testthat/test-deisotoper.R
+++ tests/testthat/test-deisotoper.R
@@ -1,8 +1,8 @@
 #R
 
+context("deisotoper")
 
-test_deisotoper <-
-function(){
+test_that("isotop groups", {
     x <- list(mZ = c(726.068, 726.337, 726.589, 726.842, 727.343, 727.846, 728.346, 
         728.846, 729.348, 730.248, 730.336, 730.581, 730.836),
         intensity = c(6.77850e+03, 2.81688e+04, 6.66884e+04, 1.22032e+07, 
@@ -13,8 +13,7 @@ function(){
     out1 <- .Call("deisotoper_main", x$mZ, x$intensity, Z=1:4, averagine, 
         massError=0.01, PACKAGE="protViz")
 
-    checkEqualsNumeric(out1$result[[1]][[1]], c(1, 4, 6, 8), tolerance=1.0e-6)
-    checkEqualsNumeric(out1$result[[2]][[1]], c(1, 3, 4, 5, 6, 7, 8), tolerance=1.0e-6)
-}
+    expect_equal(out1$result[[1]][[1]], c(1, 4, 6, 8), tolerance=1e-06)
+    expect_equal(out1$result[[2]][[1]], c(1, 3, 4, 5, 6, 7, 8), tolerance=1e-06)
+})
 
-test_deisotoper()
